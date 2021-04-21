42785a442546b84d4d78929d297b9e28454b0d8d
ANeptx <s6135512056@phuket.psu.ac.th>
2021-04-21 23:10:34 +0700

    routeredit

--- lib/router.dart
+++ lib/router.dart
@@ -5,7 +5,7 @@ import 'package:flutter_application_1/states/my_service_adopter.dart';
 import 'package:flutter_application_1/states/my_service_user.dart';
 
 class PageRouter {
-final Map<String, WidgetBuilder> map = {
+  static final Map<String, WidgetBuilder> map = {
   '/authen': (BuildContext context) => Authen(),
   '/createAccount': (BuildContext context) => CreateAccount(),
   '/myServiceUser': (BuildContext context) => MyServiceUser(),
